6ef5815a17203c3d4244bd93d07a0cc6762e9e7d
Vasily Inkovskiy <76574837+xxArchitect@users.noreply.github.com>
2022-10-01 19:39:16 -0400

    Add SearchBar component to the home page

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -3,10 +3,12 @@ import { Fragment } from "react";
 import LandingPage from "../components/LandingPage";
 import TopRatedBooksPage from "../components/TopRatedBooksPage";
 import BookSearchByGenre from "../components/BookSearchByGenre";
+import SearchBar from "../components/UI/SearchBar";
 
 export default function HomePage() {
   return (
     <Fragment>
+      <SearchBar />
       <LandingPage />
       <TopRatedBooksPage />
       <BookSearchByGenre />
