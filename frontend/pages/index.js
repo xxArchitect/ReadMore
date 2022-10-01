d5c67f41affa90baab19dd4768ad0396e69d654c
Vasily Inkovskiy <76574837+xxArchitect@users.noreply.github.com>
2022-10-01 18:38:00 -0400

    Add other sections to the home page

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,6 +1,15 @@
 // read-more.com
+import { Fragment } from "react";
 import LandingPage from "../components/LandingPage";
+import TopRatedBooksPage from "../components/TopRatedBooksPage";
+import BookSearchByGenre from "../components/BookSearchByGenre";
 
 export default function HomePage() {
-  return <LandingPage />;
+  return (
+    <Fragment>
+      <LandingPage />
+      <TopRatedBooksPage />
+      <BookSearchByGenre />
+    </Fragment>
+  );
 }
