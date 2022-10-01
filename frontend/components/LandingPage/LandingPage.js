b4be7ad64f414545ea5404a727551f18fb9f8c46
Vasily Inkovskiy <76574837+xxArchitect@users.noreply.github.com>
2022-10-01 18:37:41 -0400

    Move styling for sections to the Layout.modules.css

--- frontend/components/LandingPage/LandingPage.js
+++ frontend/components/LandingPage/LandingPage.js
@@ -1,5 +1,5 @@
 import classes from "./LandingPage.module.css";
 
-export default function LandingPage(props) {
+export default function LandingPage() {
   return <section className={classes.landingPage}>Search books...</section>;
 }
