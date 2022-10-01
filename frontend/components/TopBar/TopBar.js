cf2d198ec4dc12157faaeb20394b8fbb864935c1
Vasily Inkovskiy <76574837+xxArchitect@users.noreply.github.com>
2022-10-01 17:28:16 -0400

    Add styling for the TopBar component

--- frontend/components/TopBar/TopBar.js
+++ frontend/components/TopBar/TopBar.js
@@ -1,10 +1,16 @@
+import classes from "./TopBar.module.css";
+
 export default function TopBar() {
   return (
-    <nav>
-      <h1>ReadMore.</h1>
-      <button>RATINGS</button>
-      <button>SAVED</button>
-      <button>LOG OUT</button>
+    <nav className={classes.topBar}>
+      <h1 className={classes.logo}>ReadMore.</h1>
+      <div className={classes.buttons}>
+        <button>RATINGS</button>
+        <div className={classes.circle} />
+        <button>SAVED</button>
+        <div className={classes.circle} />
+        <button>LOG OUT</button>
+      </div>
     </nav>
   );
 }
