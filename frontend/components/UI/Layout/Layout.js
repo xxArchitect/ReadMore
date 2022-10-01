7e429e69d5f3837fb65224d9baed9242c258cde9
Vasily Inkovskiy <76574837+xxArchitect@users.noreply.github.com>
2022-10-01 18:35:57 -0400

    Add styling for sections to the Layout component

--- frontend/components/UI/Layout/Layout.js
+++ frontend/components/UI/Layout/Layout.js
@@ -1,6 +1,9 @@
 import TopBar from "../../TopBar";
 import classes from "./Layout.module.css";
 
+/**
+ * Renders main component which contains the top bar.
+ * */
 export default function Layout({ children }) {
   return (
     <main className={classes.mainContent}>
