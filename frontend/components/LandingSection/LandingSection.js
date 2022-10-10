e18ffc8daf8021c50e971d365ce433051aa29bbf
Vasily Inkovskiy <76574837+xxArchitect@users.noreply.github.com>
2022-10-10 13:48:39 -0400

    Add book cover animation to the landing section of the home page

--- frontend/components/LandingSection/LandingSection.js
+++ frontend/components/LandingSection/LandingSection.js
@@ -1,5 +1,33 @@
+import { memo } from "react";
 import classes from "./LandingSection.module.css";
+import BookCover from "../UI/BookCover";
 
-export default function LandingSection(props) {
-  return <section className={classes.landingPage}>{props.children}</section>;
+// Array containing paths to all displayed images
+const imageNames = Array.from({ length: 21 }).map(
+  (_, index) => `/sample_book_covers/sample_book_cover_${index}.webp`
+);
+
+function LandingSection(props) {
+  // Create <Image /> element for each displayed image
+  const images = imageNames.map((imageName) => (
+    <BookCover src={imageName} key={Math.random()} />
+  ));
+
+  return (
+    <section className={classes.landingPage}>
+      <div className={[classes.bookRow, classes.leftScroll1].join(" ")}>
+        {Array.from({ length: 3 }).map(() => images.slice(0, 7))}
+      </div>
+      <div className={[classes.bookRow, classes.rightScroll].join(" ")}>
+        {Array.from({ length: 3 }).map(() => images.slice(7, 14))}
+      </div>
+      <div className={[classes.bookRow, classes.leftScroll2].join(" ")}>
+        {Array.from({ length: 3 }).map(() => images.slice(14))}
+      </div>
+
+      {props.children}
+    </section>
+  );
 }
+
+export default memo(LandingSection);
